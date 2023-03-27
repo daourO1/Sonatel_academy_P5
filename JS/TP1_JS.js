998fac62c603cd061abaa3ac16a9a6de8907f191
daour01 <daoursambandaw@gmail.com>
2023-03-27 15:22:39 +0000

    modif

---
+++ JS/TP1_JS.js
@@ -0,0 +1,5 @@
+let btn = document.getElementById("btn_add");
+btn.addEventListener("click", btn_add);
+function btn_add() {
+    alert("ok")
+}
---
+++ JS/test1.js
@@ -0,0 +1,3 @@
+const someNinthVar = 9
+someNinthVar = 10
+console.log(someNinthVar)
