a4a3622167c53a6db891c705958754c131f3cbdb
daour01 <daoursambandaw@gmail.com>
2023-04-10 20:33:04 +0000

    modif

--- JS/TP7/TP7_JS.js
+++ JS/TP7/TP7_JS.js
@@ -2,8 +2,8 @@
 
 const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=57'
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-let cont_image = document.getElementById("cont_image");
-let section = document.getElementsByTagName("section")
+// let cont_image = document.getElementById("cont_image");
+let section = document.querySelector("section")
 
 function catologue () {
 
@@ -15,34 +15,51 @@ fetch(APIURL)
     })
     .then(function(donnee){
         
-        for (let element of donnee.results) {
-            
+        donnee.results.forEach(element => {
+            console.log(element)
+            let cont_image = document.createElement("div");
+            cont_image.id = "cont_image";
             // Afficher l'image
             let divImage = document.createElement("div");
             let img = document.createElement("img");
             img.src = `https://image.tmdb.org/t/p/w1280${element.poster_path}`;
             img.alt = "film";
-            img.id = "photo"
+            divImage.id = "photo";
+
+            // description
+            let description = document.createElement("div");
+            description.id = "description";
+            description.innerHTML = `${element.overview}`;
+
+
             // Afficher le titre
             let texte = document.createElement("div");
-            texte.id = "texte"
+            texte.id = "texte";
             let titre = document.createElement("div");
-            titre.innerHTML = `${element.original_title}`
+            titre.id = "titre"
+            titre.innerHTML = `${element.original_title}`;
+
+            
             // Afficher les votes
             let vote = document.createElement("div")
+            vote.id = "vote"
             vote.innerHTML = `${element.vote_average}`
-
+            
             divImage.appendChild(img);
-            console.log(divImage)
+            divImage.appendChild(description);
             texte.appendChild(titre);
             texte.appendChild(vote);
             cont_image.appendChild(divImage);
             cont_image.appendChild(texte);
-            // section.appendChild(cont_image)
+            section.appendChild(cont_image);
             
+        })
+    })  
+
+            
+
+
 
             
             
-            
-        }
-    })
+     
