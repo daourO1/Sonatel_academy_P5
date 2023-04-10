ba65713a794a00051b71564196611c88a5bcc492
daour01 <daoursambandaw@gmail.com>
2023-04-10 23:26:15 +0000

    modif

---
+++ Projet JS/projet_JS.js
@@ -0,0 +1,14 @@
+const tabJours = {
+    "LUNDI":[],
+    "MARDI":[],
+    "MERCREDI":[],
+    "JEUDI":[],
+    "VENDREDI":[],
+    "SAMEDI":[],
+}
+const jours_cours = document.getElementById("jours_cours");
+for (const jour in tabJours) {
+    var jours = document.createElement("div");
+    var cours = document.createElement("div");
+    
+}
