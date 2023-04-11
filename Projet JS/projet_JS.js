17077f92c6fae20c886ed93d7d1342caaefdbd65
daour01 <daoursambandaw@gmail.com>
2023-04-11 15:05:27 +0000

    modif

--- Projet JS/projet_JS.js
+++ Projet JS/projet_JS.js
@@ -1,14 +1,33 @@
-const tabJours = {
-    "LUNDI":[],
-    "MARDI":[],
-    "MERCREDI":[],
-    "JEUDI":[],
-    "VENDREDI":[],
-    "SAMEDI":[],
-}
-const jours_cours = document.getElementById("jours_cours");
-for (const jour in tabJours) {
-    var jours = document.createElement("div");
-    var cours = document.createElement("div");
+
+const tabJours = [
+    "LUNDI",
+    "MARDI",
+    "MERCREDI",
+    "JEUDI",
+    "VENDREDI",
+    "SAMEDI"
+]
     
+const jours_cours = document.getElementById("jours_cours");
+for (var jour in tabJours) {
+    var jours = document.createElement("div");
+    jours.innerHTML = tabJours[jour];
+    jours.className = "jrs";
+    var cours = document.createElement("div");
+    cours.className = "crs";
+    var emplois = document.createElement("div");
+    emplois.appendChild(jours);
+    emplois.appendChild(cours);
+    emplois.id = "emplois"
+    jours_cours.appendChild(emplois);
+    
+    
+}
+const list_enseign = document.getElementById("enseignant");
+const enseign = ['ALy','Mbaye','One','Diouf'];
+for ( i=0 ; i<enseign.length ; i++) {
+    var option = document.createElement("option");
+    option.value="texte";
+    option.textContent = enseign[i];
+    list_enseign.appendChild(option);
 }
